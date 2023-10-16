992f07d6ef9e3815495e7f1f4548395c419a121d
gpendyal <gpendyal@buffalo.edu>
2023-10-16 01:08:56 -0400

    airplane cursor added

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,22 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { faPlane } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function App() {
+  const cursorStyle = {
+    position: 'absolute',
+    cursor: 'none', // Hide the default cursor
+  };
+
+  const cursorSize = {
+    width: '48px', // Increase the width
+    height: '48px', // Increase the height
+  };
+
+  const updateCursorPosition = (e) => {
+    const cursor = document.getElementById('customCursor');
+    cursor.style.left = `${e.clientX - 24}px`;  // Adjust these values accordingly
+    cursor.style.top = `${e.clientY - 24}px`;  // Adjust these values accordingly
+  };
+
+  window.addEventListener('mousemove', updateCursorPosition);
+
   return (
     <div className="App">
-      <head>
-        <title>Page Title Goes Here</title>
-      </head>
-      <body className="App-header">
-
-
-
-
-
-
-
-
-      </body>
+      <div id="customCursor" style={cursorStyle}>
+        <FontAwesomeIcon icon={faPlane} style={cursorSize} />
+      </div>
+      {/* Your application content */}
     </div>
   );
 }
